3ac4ba6f857428580ec20b9bcc8ebb1208f28973
Tabben <nitharsan.nagularajan@yh.nackademin.se>
2021-05-27 12:37:57 +0200

    Added stockRoute, removed redundant line in reportcontroller

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 bodyParser = require('body-parser')
+
 const db = require('./Database/Db')
 const reportRoute = require('./Routes/ReportRoute')
 const userRoute = require('./Routes/UserRoute')
+const stockRoute = require('./Routes/StockRoute')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -12,6 +14,7 @@ app.use(cors())
 
 app.use('/reports', reportRoute)
 app.use('/user', userRoute)
+app.use('/stocks', stockRoute)
 
 db.connect()
 
---
+++ Routes/StockRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const stockController = require('../Controllers/StockController')
+
+router.get('/', stockController.getStocks)
+
+router.post('/', stockController.addStock)
+
+router.patch('/:stockID', stockController.editStock)
+
+router.delete('/:stockID', stockController.deleteStock)
+
+router.get('/:stockID', stockController.getStock)
+
+module.exports = router
